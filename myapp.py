0a2881b00750e814713cccabc14a951f44eba303
liuxiaoguang <vincexgliu@gamil.com>
2017-12-12 10:17:32 +0800

    add config

--- myapp.py
+++ myapp.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_restful import Api
 from app.resources import Email
+from flask_mail import Mail
 
 
 app = Flask(__name__)
+mail = Mail(app)
 api = Api(app)
 api.add_resource(Email, '/emails')
     
